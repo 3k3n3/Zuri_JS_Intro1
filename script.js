aafd10d62805e7486a80353e7837fbb5fb81736d
Ekene C <clarencekene@outlook.com>
2022-06-24 02:27:07 +0100

    script and index

---
+++ script.js
@@ -0,0 +1,16 @@
+//Output Name, Height and Country
+
+// // using variables
+// const myname = 'Ekene Clarence';
+// let height = 5.7;
+// let country = 'Nigeria';
+
+// alert('Name: ' + myname +'\nHeight: ' + height + 'ft \nCountry: ' + country)
+
+
+//using function
+function userInfo(myname, height, country) {
+    alert('Name: ' + myname +'\nHeight: ' + height + 'ft \nCountry: ' + country)
+}
+
+userInfo("Ekene Clarence", 5.7, "Nigeria")
